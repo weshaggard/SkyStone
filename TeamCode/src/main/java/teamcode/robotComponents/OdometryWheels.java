a5a0637559d79857af3d88f84de048dd8a60c820
MrJamesGaming <52509738+MrJamesGaming@users.noreply.github.com>
2019-11-19 16:32:50 -0800

    Odometry

--- TeamCode/src/main/java/teamcode/robotComponents/OdometryWheels.java
+++ TeamCode/src/main/java/teamcode/robotComponents/OdometryWheels.java
@@ -5,28 +5,45 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import java.util.List;
 
+import teamcode.common.AbstractOpMode;
 import teamcode.common.Vector2D;
 
 
 public class OdometryWheels {
+    private static final double DEGREE_ERROR = 3.0;
     private DcMotor XWheel;
     private DcMotor YWheel;
     private List<Vector2D> compositeComponents;
+    private List<Double> magnitudes;
     private List<Vector2D> path;
     private long apex;
+    private double distanceX;
+    private double distanceY;
     private int compositeTickX;
     private int compositeTickY;
+    private final double DISTANCE_APART_INCHES  = 1;
+    private final TTDriveSystem driveSystem;
 
+
+
+    /* Ok so just a notepad for myself on Odometry
+    youve got a composite of Vectors and an ideal composite path
+
+    Interpret each vector as a line and find a slope, use that to determine points and thus distance away from the ideal
+    Hopefully future me knows what the heck I meant
+     */
 
     public OdometryWheels(HardwareMap hardwareMap, List<Vector2D> path){
         XWheel = hardwareMap.get(DcMotor.class, "Xwheel Odometry");
         YWheel = hardwareMap.get(DcMotor.class, "YWheel Odometry");
         apex = System.currentTimeMillis();
         this.path = path;
+        driveSystem = new TTDriveSystem(hardwareMap);
         XWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         YWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         XWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         YWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
     }
 
     public void updateComposite(){
@@ -42,15 +59,21 @@ public class OdometryWheels {
         comparePaths();
     }
 
+
     private void comparePaths() {
         for(int i = 0; i < compositeComponents.size(); i++){
-            if(compositeComponents.get(i).getDirection() == path.get(i).getDirection()){
+            if(compositeComponents.get(i).getDirection() + DEGREE_ERROR == path.get(i).getDirection() || compositeComponents.get(i).getDirection() - DEGREE_ERROR == path.get(i).getDirection()){
                 //good case, nothing is necessary
             }else{
                 double direction = path.get(i).angleBetween(compositeComponents.get(i));
+                double distance = compositeComponents.get(i).magnitude();
+                driveSystem.turn(direction, 1);
+                //can change speed if necessary
 
+                driveSystem.vertical(-distance, 1);
                 //bad case, need to self correct.
             }
         }
     }
+
 }
