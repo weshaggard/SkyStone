c30e022828660b92d047ae3466b0ec618c40e6c6
PrimeTactic <30270951+PrimeTactic@users.noreply.github.com>
2019-12-02 20:54:18 -0800

    Arc code
    Somewhat working arc code.

--- TeamCode/src/main/java/teamcode/test/calibration/ArcTest.java
+++ TeamCode/src/main/java/teamcode/test/calibration/ArcTest.java
@@ -22,6 +22,7 @@ public class ArcTest extends AbstractOpMode {
     protected void onStart() {
         Debug.log("here");
         driveSystem.frontArc(true, 0.6,90, 9);
+        while (opModeIsActive());
     }
 
     @Override
