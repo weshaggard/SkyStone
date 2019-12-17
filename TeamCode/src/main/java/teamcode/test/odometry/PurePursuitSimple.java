332851f0d74b71f9e451ef8a0bd07f0c1f315747
MrJamesGaming <52509738+MrJamesGaming@users.noreply.github.com>
2019-12-16 23:06:47 -0800

    Substantial Progress on TeleOp, optimizations are the only thing necessary if all works correctly

---
+++ TeamCode/src/main/java/teamcode/test/odometry/PurePursuitSimple.java
@@ -0,0 +1,45 @@
+package teamcode.test.odometry;
+
+import java.util.ArrayList;
+
+import teamcode.common.Debug;
+import teamcode.common.Point;
+import teamcode.common.Vector2D;
+import teamcode.league3.Constants;
+import teamcode.league3.DriveSystem;
+
+public class PurePursuitSimple {
+    private ArrayList<Point> pointsVisited;
+    private OdometryWheelsFinal wheels;
+    private DriveSystem driveSystem;
+
+    public PurePursuitSimple(OdometryWheelsFinal wheels, DriveSystem driveSystem){
+        this.wheels = wheels;
+        this.driveSystem = driveSystem;
+    }
+
+
+    public  void followCurve(ArrayList<Point> path, double power){
+        for(int i = 0; i < path.size() - 1; i++){
+
+            goToPosistion(path.get(i), path.get(i + 1), power);
+        }
+
+    }
+    public void goToPosistion(Point startPoint, Point endPoint, double power){
+        double deltaX = endPoint.x - startPoint.x;
+        double deltaY = endPoint.y - startPoint.y;
+        double distanceHypotenuse = Math.hypot(deltaY, deltaX);
+        double xPower = deltaX / distanceHypotenuse * power;
+        double yPower = deltaY / distanceHypotenuse * power;
+        double absoluteAngleRads = Math.atan2(deltaY, deltaX);
+        double relativeAngleRads = wheels.angleWrapper(absoluteAngleRads - wheels.currentGlobalDirection);
+        driveSystem.rotate(Math.toDegrees(relativeAngleRads), Constants.TURN_SPEED);
+        Vector2D velocity = new Vector2D(yPower, xPower);
+        while(!driveSystem.nearTarget()){
+            driveSystem.continuous(velocity, 0);
+
+        }
+        driveSystem.brake();
+    }
+}
---
+++ TeamCode/src/main/java/teamcode/test/odometry/PurePursuitSimpleTest.java
@@ -0,0 +1,49 @@
+package teamcode.test.odometry;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import java.util.ArrayList;
+
+import teamcode.common.AbstractOpMode;
+import teamcode.common.Point;
+import teamcode.league3.DriveSystem;
+
+
+@Autonomous(name = "Pursuit Test")
+public class PurePursuitSimpleTest extends AbstractOpMode {
+    DriveSystem driveSystem;
+    OdometryWheelsFinal wheels;
+    PurePursuitSimple movement;
+    ArrayList<Point> path;
+
+    @Override
+    protected void onInitialize() {
+        driveSystem = new DriveSystem(AbstractOpMode.currentOpMode().hardwareMap);
+        wheels = new OdometryWheelsFinal(this, new Point(100,100),driveSystem,0);
+        movement = new PurePursuitSimple(wheels, driveSystem);
+        path = new ArrayList<>();
+    }
+
+    @Override
+    protected void onStart() {
+        Thread odometerUpdate = new Thread(){
+            @Override
+            public void run(){
+                wheels.update();
+
+            }
+        };
+        odometerUpdate.start();
+        path.add(new Point(100, 100));
+        path.add(new Point(300,100));
+        path.add(new Point(310, 100));
+        movement.followCurve(path, 0.5);
+
+    }
+
+    @Override
+    protected void onStop() {
+
+    }
+}
